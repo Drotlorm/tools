1162f95047c990aefd949f8ee3fd747f4e82b474
tekmans <tekmans@no-log.org>
2018-07-13 10:16:05 +0200

    (Bugfix) Add MasteredOn tables and dump personal bootservice To see master after migration Dump personal bootservice start >12

--- dev/migration_scripts/export_imaging_db_and_data.sh
+++ dev/migration_scripts/export_imaging_db_and_data.sh
@@ -4,10 +4,14 @@ DESTINATION_ADDRESS='root@192.168.56.2'
 
 # Dump database contents
 echo "Exporting imaging database..."
-mysqldump --compact --complete-insert --no-create-info --where='id>=1000' imaging BootService PostInstallScript |grep ^INSERT >> imaging.sql
-mysqldump --compact --complete-insert --no-create-info imaging BootServiceOnImagingServer PostInstallScriptOnImagingServer |grep ^INSERT >> imaging.sql
 mysqldump --compact --complete-insert --no-create-info --where='is_master=1' imaging Image |grep ^INSERT >> imaging.sql
 mysqldump --compact --complete-insert --no-create-info imaging ImageOnImagingServer |grep ^INSERT >> imaging.sql
+mysqldump --compact --complete-insert --no-create-info imaging MasteredOn |grep ^INSERT >> imaging.sql
+
+mysqldump --compact --complete-insert --no-create-info --where='id>=13' imaging BootService |grep ^INSERT >> imaging.sql
+mysqldump --compact --complete-insert --no-create-info --where='id>=1000' imaging PostInstallScript |grep ^INSERT >> imaging.sql
+mysqldump --compact --complete-insert --no-create-info imaging BootServiceOnImagingServer PostInstallScriptOnImagingServer |grep ^INSERT >> imaging.sql
+
 scp imaging.sql ${DESTINATION_ADDRESS}:/root
 
 # Copy masters
@@ -28,4 +32,5 @@ echo "Copying sysprep files..."
 scp /var/lib/pulse2/imaging/postinst/sysprep/*.xml ${DESTINATION_ADDRESS}:/var/lib/pulse2/imaging/postinst/sysprep/
 
 # End tasks
+scp imaging.sql ${DESTINATION_ADDRESS}:/root
 echo "/root/imaging.sql needs to be imported to imaging database on target Pulse"
