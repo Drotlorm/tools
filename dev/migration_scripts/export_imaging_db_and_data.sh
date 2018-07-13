a5a282b51ad182bc341438d6586fb18a8bdf56d6
spointu <spointu@gmail.com>
2018-07-13 11:01:52 +0200

    Fix migration of imaging database

--- dev/migration_scripts/export_imaging_db_and_data.sh
+++ dev/migration_scripts/export_imaging_db_and_data.sh
@@ -4,22 +4,23 @@ DESTINATION_ADDRESS='root@192.168.56.2'
 
 # Dump database contents
 echo "Exporting imaging database..."
-mysqldump --compact --complete-insert --no-create-info --where='is_master=1' imaging Image |grep ^INSERT >> imaging.sql
-mysqldump --compact --complete-insert --no-create-info imaging ImageOnImagingServer |grep ^INSERT >> imaging.sql
-mysqldump --compact --complete-insert --no-create-info imaging MasteredOn |grep ^INSERT >> imaging.sql
-
+echo "set FOREIGN_KEY_CHECKS = 0;" > imaging.sql
+echo "START TRANSACTION;" >> imaging.sql
 mysqldump --compact --complete-insert --no-create-info --where='id>=13' imaging BootService |grep ^INSERT >> imaging.sql
 mysqldump --compact --complete-insert --no-create-info --where='id>=1000' imaging PostInstallScript |grep ^INSERT >> imaging.sql
 mysqldump --compact --complete-insert --no-create-info imaging BootServiceOnImagingServer PostInstallScriptOnImagingServer |grep ^INSERT >> imaging.sql
-
-scp imaging.sql ${DESTINATION_ADDRESS}:/root
+mysqldump --compact --complete-insert --no-create-info --where='is_master=1' imaging Image |grep ^INSERT >> imaging.sql
+mysqldump --compact --complete-insert --no-create-info imaging ImageOnImagingServer |grep ^INSERT >> imaging.sql
+mysqldump --compact --complete-insert --no-create-info --lock-all-tables --where='fk_image in (select id from Image where is_master=1)' imaging MasteredOn |grep ^INSERT >> imaging.sql
+echo "COMMIT;" >> imaging.sql
+echo "set FOREIGN_KEY_CHECKS = 1;" >> imaging.sql
 
 # Copy masters
 echo "Copying masters..."
 PATHS=$(echo 'SELECT path FROM Image WHERE is_master=1;' | mysql imaging)
 readarray MASTER <<<"$PATHS"
 for (( i=1; i<=$(( ${#MASTER[@]}-1 )); i++ ))
-do 
+do
 	rsync -av ${MASTER[$i]} ${DESTINATION_ADDRESS}:/var/lib/pulse2/imaging/masters/
 done
 
