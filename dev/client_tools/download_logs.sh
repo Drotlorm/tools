ecbb9f00158070498ec7000a46cfc48dc3689997
spointu <spointu@gmail.com>
2018-09-19 09:26:46 +0200

    Improving client debug scripts

--- dev/client_tools/download_logs.sh
+++ dev/client_tools/download_logs.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+tmpfolder="/tmp"
+identityfile="/root/.ssh/id_rsa"
+
 ip_addresses_list=$(echo "select distinct name from glpi_ipaddresses where version=4;" | mysql -s glpi)
 
 ip_address_error=''
+
 for ip_address in ${ip_addresses_list}; do
 	echo "Contacting ${ip_address}"
 	ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 	pulse@${ip_address} hostname
 	if [ $? -eq 0 ]; then
 		echo "Downloading xmpp-agent.log"
-		mkdir -p /tmp/xmpp-logs/${ip_address}/
-		scp -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
-		pulse@${ip_address}:"\"c:\\Program Files\\Pulse\\var\\log\\xmpp-agent.log\"" /tmp/xmpp-logs/${ip_address}/
+		mkdir -p ${tmpfolder}/${ip_address}/
+		scp -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+		pulse@${ip_address}:"\"c:\\Program Files\\Pulse\\var\\log\\xmpp-agent.log\"" ${tmpfolder}/${ip_address}/
 		echo ""
 		echo "***********************************"
 		echo ""
@@ -20,7 +24,7 @@ for ip_address in ${ip_addresses_list}; do
 	fi
 done
 
-if [[ ${ip_address_error} != '' ]]; then
+if [ "${ip_address_error}" != '' ]; then
     echo "############################################################"
     echo "The following machines could not be contacted: ${ip_address_error}"
     echo "############################################################"
--- dev/client_tools/restart_agent.sh
+++ dev/client_tools/restart_agent.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+identityfile="/root/.ssh/id_rsa"
+
 ip_addresses_list=$(echo "select distinct name from glpi_ipaddresses where version=4;" | mysql -s glpi)
 
 ip_address_error=''
+
 for ip_address in ${ip_addresses_list}; do
 	echo "Contacting ${ip_address}"
-	ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+	ssh -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 	pulse@${ip_address} hostname
 	if [ $? -eq 0 ]; then
 		echo "Killing Pulse Agent"
-		ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+		ssh -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 		pulse@${ip_address} schtasks /End /TN "\"Pulse Agent\""
-		ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+		ssh -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 		pulse@${ip_address} taskkill /F /IM python.exe
 		sleep 3
 		echo ""
 		echo "Restarting Pulse Agent"
-		ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+		ssh -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 		pulse@${ip_address} schtasks /Run /TN "\"Pulse Agent\""
 		sleep 2
-		ssh -o IdentityFile=/root/.ssh/id_rsa -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
+		ssh -o IdentityFile=${identityfile} -o StrictHostKeyChecking=no -o Batchmode=yes -o PasswordAuthentication=no -o ServerAliveInterval=10 -o CheckHostIP=no -o ConnectTimeout=10 \
 		pulse@${ip_address} schtasks /Query /TN "\"Pulse Agent\""
 		echo ""
 		echo "***********************************"
@@ -29,7 +32,7 @@ for ip_address in ${ip_addresses_list}; do
 	fi
 done
 
-if [[ ${ip_address_error} != '' ]]; then
+if [ "${ip_address_error}" != '' ]; then
     echo "############################################################"
     echo "The following machines could not be contacted: ${ip_address_error}"
     echo "############################################################"
